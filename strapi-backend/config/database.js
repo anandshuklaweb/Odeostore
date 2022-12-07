e4e7780892523a7d1d2b3d438e3b11cdb6e30f95
Anand Shukla <anandshukla.web@gmail.com>
2022-12-07 22:42:10 +0530

    Initial Commit

---
+++ strapi-backend/config/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "odeostore"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "root"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+    },
+    debug: false,
+  },
+});
